06eb78de916f8f88f7b02137607335dfa16eb356
Voldemar-imp <108013210+Voldemar-imp@users.noreply.github.com>
2022-06-23 18:16:07 +0300

    Update Program8.cs

--- Program8.cs
+++ Program8.cs
@@ -13,17 +13,17 @@ namespace Task7
             int patientsInLine;
             int receptionTime = 10;
             int minutesPerHour = 60;
-            int minutes;
-            int hours;
+            int minutesOfWaiting;
+            int hoursOfWaiting;
 
             Console.WriteLine("Добро пожаловать в поликлиннику, время приема одного пациента составлет: " + receptionTime + " минут");
             Console.Write("Введите количество пациентов в очереди:");
             patientsInLine = Convert.ToInt32(Console.ReadLine());
 
-            minutes = patientsInLine * receptionTime;
-            hours = minutes / minutesPerHour;
-            minutes -= hours * minutesPerHour;
-            Console.WriteLine("Вы должны отстоять в очереди {0} часа и {1} минут.", hours, minutes);
+            minutesOfWaiting = patientsInLine * receptionTime;
+            hoursOfWaiting = minutesOfWaiting / minutesPerHour;
+            minutesOfWaiting -= hoursOfWaiting * minutesPerHour;
+            Console.WriteLine("Вы должны отстоять в очереди {0} часа и {1} минут.", hoursOfWaiting, minutesOfWaiting);
         }
     }
 }
